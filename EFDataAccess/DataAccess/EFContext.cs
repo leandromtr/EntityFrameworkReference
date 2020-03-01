087d2693d58cc93ea994faf04f6dd610f668f0fb
LeandroReis <leandromtr@hotmail.com>
2020-03-01 22:50:17 +0000

    Install/ setup EF

---
+++ EFDataAccess/DataAccess/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using EFDataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDataAccess.DataAccess
+{
+    public class EFContext: DbContext
+    {
+        public EFContext(DbContextOptions options) : base(options) { }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> EmailAddresses { get; set; }
+        
+    }
+}
